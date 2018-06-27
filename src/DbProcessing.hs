b88e27bfaee6e203f6c1102a8c66f0c399134551
Eric Zoerner <eric.zoerner@gmail.com>
2018-06-27 12:50:03 -0700

    removed solution

--- src/DbProcessing.hs
+++ src/DbProcessing.hs
@@ -11,9 +11,4 @@ data DatabaseItem = DbString String
 --    Write a function that filters for DbDate values and returns a list
 --    of the UTCTime values inside them.
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate db =
-  let
-    f ts (DbDate t) = t : ts
-    f ts _ = ts
-  in
-    foldl f [] db
+filterDbDate = undefined
