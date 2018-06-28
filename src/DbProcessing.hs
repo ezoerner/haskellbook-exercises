22360dde97699a0cc3ba1d3157a16ce3ec1f5268
Eric Zoerner <eric.zoerner@nike.com>
2018-06-28 14:25:58 -0700

    added haddock comment

--- src/DbProcessing.hs
+++ src/DbProcessing.hs
@@ -1,3 +1,11 @@
+{-|
+Module      : DbProcessing
+Description : Exercise "Database Processing" from the Haskell Book
+Maintainer  : eric.zoerner@gmail.com
+
+Exercise "Database Processing" from Chapter 10
+of /Haskell Programming from First Principles/
+-}
 module DbProcessing where
 
 import Data.Time
@@ -8,7 +16,8 @@ data DatabaseItem = DbString String
                   | DbDate   UTCTime
     deriving (Eq, Ord, Show)
 
---    Write a function that filters for DbDate values and returns a list
+-- |  Write a function that filters for DbDate values
+--    and returns a list
 --    of the UTCTime values inside them.
 filterDbDate :: [DatabaseItem] -> [UTCTime]
 filterDbDate = undefined
