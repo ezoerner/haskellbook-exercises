35f610133f7b9b4f6db18f3e16ad5c5f793922dd
Fernando Freire <fernando.freire@nike.com>
2018-07-12 15:41:13 -0700

    Add mob programming solution to ex.10.4

--- src/DbProcessing.hs
+++ src/DbProcessing.hs
@@ -1,14 +1,17 @@
 {-|
 Module      : DbProcessing
 Description : Exercise "Database Processing" from the Haskell Book
-Maintainer  : eric.zoerner@gmail.com
+Maintainer  : the.mob@nike.com
 
 Exercise "Database Processing" from Chapter 10
 of /Haskell Programming from First Principles/
 -}
+
 module DbProcessing where
 
 import Data.Time
+import Data.Maybe (mapMaybe)
+import Data.Monoid
 
 
 data DatabaseItem = DbString String
@@ -23,13 +26,28 @@ filterDbDate :: [DatabaseItem] -> [UTCTime]
 filterDbDate = undefined
 
 -- | Write a function that sums all of the DbNumber values.
+-- We want to reduce some shit, get an integer
 sumDb :: [DatabaseItem] -> Integer
-sumDb = undefined
+sumDb [] = 0
+sumDb (DbNumber num : xs) = num + sumDb xs
+sumDb (_ : xs) = sumDb xs
+
+-- The same, but with higher order functions, yo
+sumDb' :: [DatabaseItem] -> Integer
+sumDb' items = sum $ mapMaybe f items
+  where f :: DatabaseItem -> Maybe Integer
+        f (DbNumber x) = Just x
+        f _          = Nothing
+
+
+-- Monoid -- binary operation with an identity
+-- Addition under positive numbers is a monoid -- 1 + 0 = 1; 1 + 1 = 2; ...
+sumDb'' :: (Foldable t) => t DatabaseItem -> Integer
+sumDb'' = getSum . foldMap (\x -> case x of DbNumber y -> Sum y; _ -> 0)
 
 -- | Write a function that gets the average of the DbNumber values.
 
 -- You'll probably need to use fromIntegral
 -- to get from Integer to Double.
-
 avgDb :: [DatabaseItem] -> Double
 avgDb = undefined
--- test/DbProcessingSpec.hs
+++ test/DbProcessingSpec.hs
@@ -2,7 +2,7 @@ module DbProcessingSpec where
 
 import Data.Time
 import Test.Tasty.HUnit
-import Data.List (sort)
+--import Data.List (sort)
 
 import DbProcessing
 
@@ -27,12 +27,12 @@ time1 = UTCTime (fromGregorian 1911 5 1) diffTime
 time2 :: UTCTime
 time2 = UTCTime  (fromGregorian 1921 5 1) diffTime
 
-unit_filterDbDate :: IO ()
-unit_filterDbDate = sort (filterDbDate theDatabase) @?=
-    sort [time1, time2]
+{-unit_filterDbDate :: IO ()-}
+{-unit_filterDbDate = sort (filterDbDate theDatabase) @?=-}
+    {-sort [time1, time2]-}
 
 unit_sumDb :: IO ()
-unit_sumDb = sumDb theDatabase @?= 9876
+unit_sumDb = sumDb' theDatabase @?= 9876
 
 unit_avgDb :: IO ()
 unit_avgDb = avgDb theDatabase @?= 3292
